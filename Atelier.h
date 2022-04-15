535fccdc79b8e5267eba5cee7cb0ff497a4910ff
Mihai-02 <mihai_ciocan02@yahoo.com>
2022-04-15 20:48:33 +0300

    Minor changes

--- Atelier.h
+++ Atelier.h
@@ -11,7 +11,7 @@ class Atelier
 
     public:
         Atelier(string name={}){nume=name;}
-        void give_name(){cout << "Introduceti un nume pentru atelier: "; cin >> nume;}
+        void give_name(){cout << "Introduceti un nume pentru atelier: "; getline(cin, nume);}
         void print_info() const
         {
             cout << "Numele atelierului: " << nume << "\n";
